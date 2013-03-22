631f6be3f061c6a72a4a58384f1cca05e0f6ac86
India Meisner <india.rae@gmail.com>
2013-03-22 10:00:50 -0700

    Initial commit

---
+++ board.rb
@@ -0,0 +1,10 @@
+class Board
+  
+  attr_accessor :board
+  
+  def initialize
+    
+  end
+  
+  
+end
---
+++ checkers.rb
@@ -0,0 +1,10 @@
+class Checkers
+  
+#initialize the board  
+  
+  
+#run the game
+
+
+  
+end
