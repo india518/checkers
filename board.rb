bcfc17bc66f8f243562e596c111217662567912d
India Meisner <india.rae@gmail.com>
2013-03-22 11:23:35 -0700

    refactor Piece class, add display

--- board.rb
+++ board.rb
@@ -1,10 +1,42 @@
 class Board
   
-  attr_accessor :board
+  attr_accessor :grid
   
   def initialize
-    @grid = Array.new(8) { Array.new(8) {nil} }
+    self.grid = Array.new(8) { Array.new(8) }
+    place_red_pieces
+    place_black_pieces
   end
   
+  def place_red_pieces
+    [0,2,4,6].each do |y|
+      self.grid[0][1+y] = Piece.new(:red, [0, 1+y])
+      self.grid[1][0+y] = Piece.new(:red, [1, 0+y])
+      self.grid[2][1+y] = Piece.new(:red, [2, 1+y])
+    end
+  end
+  
+  def place_black_pieces
+    [0,2,4,6].each do |y|
+      self.grid[5][0+y] = Piece.new(:black, [5, 0+y])
+      self.grid[6][1+y] = Piece.new(:black, [6, 1+y])
+      self.grid[7][0+y] = Piece.new(:black, [7, 0+y])
+    end
+  end
+  
+  def display
+    print "    | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 |\n"
+    8.times do |x|
+      print "#{x} : "
+      8.times do |y|
+        if self.grid[x][y].nil?
+          print "|   " 
+        else
+          print "| #{self.grid[x][y].display} "
+        end
+      end
+      print "|\n"
+    end
+  end
   
 end
--- piece.rb
+++ piece.rb
@@ -1,11 +1,26 @@
 class Piece
 
-  attr_reader :color
-  attr_accessor :position
+  attr_reader :color, :display
+  attr_accessor :position, :king
   
   def initialize(color, position)
     @color = color
     @position = position
+    @king = false
+    @display = get_display
+  end
+  
+  def get_display
+    case [@color, @king]
+    when [:red, false]
+      @display = :r
+    when [:red, true]
+      @display = :R
+    when [:black, false]
+      @display = :b
+    when [:black, true]
+      @display = :B
+    end
   end
 
 end
